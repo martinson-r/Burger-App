4b05c12cd884d5da264b7c66f7c8c6c55c51d83b
Rihana Chang <martinson.r@gmail.com>
2022-06-16 13:16:22 -0700

    Initial styling for home page

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -5,7 +5,15 @@ import "./home.css";
 function Home() {
 
     return (
-        <></>
+        <div className="container">
+            <header className="header">
+                <img src="/bobs_burgers_logo.png" alt="Bob's Burgers Logo" className="logo"></img>
+                <div className="contactInfo">
+                    <div>123 Ocean Avenue, Ocean View, NJ, 07735</div>
+                    <div>(123) 456-6789</div>
+                </div>
+            </header>
+        </div>
     );
 
 }
