00c62ec669398e56c28264711191d67ebc31d4d7
Rihana Chang <martinson.r@gmail.com>
2022-06-16 14:41:19 -0700

    Add subtotal display

--- src/components/Cart/index.js
+++ src/components/Cart/index.js
@@ -1,17 +1,24 @@
 import PrimaryButton from "../PrimaryButton";
 import { useDispatch, useSelector } from "react-redux";
 import uuid from "react-uuid";
+import { removeItemFromCart } from "../../store/cart";
 import "./cart.css";
 
 function Cart() {
     const cart = useSelector(state => state.cart);
     const dispatch = useDispatch();
+    let subtotal = [];
+    cart.cart.forEach((cartItem) => subtotal.push(cartItem.label.price));
 
     return (
     <>
         <h3>MY CART</h3>
         {cart.length == 0 && <p>Add some delicious burgers!</p>}
-        {cart.cart.map((cartItem) => <div key={uuid()}><div key={uuid()} className="cartMenuItem" >{cartItem.itemName}, {cartItem.label.size} - {cartItem.label.price} </div><div className="cartMenuItem"><b>x</b></div></div>)}
+
+        {cart.cart.map((cartItem) => <div key={uuid()}><div key={uuid()} className="cartMenuItem" >{cartItem.itemName}, {cartItem.label.size} - {cartItem.label.price} </div><div className="cartMenuItem" onClick={() => dispatch(removeItemFromCart(cartItem))}><b>x</b></div></div>)}
+        <h5>SubTotal:</h5>
+        {console.log('subtotal', subtotal)}
+        <p>{subtotal.reduce((previousValue, currentValue) => previousValue + currentValue, 0)}</p>
         <PrimaryButton label="Check out" />
         <PrimaryButton label="Empty Cart" />
     </>
--- src/store/cart.js
+++ src/store/cart.js
@@ -1,5 +1,6 @@
 export const ADD_ITEM = "./ADD_ITEM";
 export const EMPTY_CART = "./EMPTY_CART";
+export const REMOVE_ITEM = "./REMOVE_ITEM";
 
 const add = (item) => ({
     type: ADD_ITEM,
@@ -10,16 +11,24 @@ const add = (item) => ({
     type: EMPTY_CART,
 });
 
+const remove = (item) => ({
+    type: REMOVE_ITEM,
+    item
+});
+
 
   export const addMenuItemToCart = (payload) => async (dispatch) => {
     const { itemName, label } = payload;
     dispatch(add({ label, itemName }));
   }
 
-
   export const emptyCart = () => async (dispatch) => {
     dispatch(empty());
 }
+
+export const removeItemFromCart = (payload) => async (dispatch) => {
+    dispatch(remove(payload));
+  }
 
 
   const initialState = {
@@ -33,6 +42,15 @@ const add = (item) => ({
             ...state,
             cart: [action.item, ...state.cart],
           }
+        }
+        case REMOVE_ITEM: {
+            // filter on an array is probably ok,
+            // because I doubt anyone is ordering 50,000 burgers
+            let filteredArray = state.cart.filter(item => item !== action.item);
+            return {
+                ...state,
+                cart: filteredArray,
+            }
         }
         case EMPTY_CART: {
             return {
