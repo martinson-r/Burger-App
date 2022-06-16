fed61b969b450f1ba862d6a18b558a3a8c190751
Rihana Chang <martinson.r@gmail.com>
2022-06-16 15:38:48 -0700

    Add feedback for number of items in cart

--- ROADMAP.md
+++ ROADMAP.md
@@ -4,6 +4,7 @@
 
 - Focused on Deliverable, and unfortunately neglected testing. I would write more tests.
 - I would structure the HTML a little better with more time - particularly the layout and placement of the shopping cart bag icon.
+- Given more time, I would add some sort of feedback, like a counter, that there is an item in your cart - right now it's not very clear.
 
 ## Features I Would Add
 
--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -10,6 +10,7 @@ function Home() {
 
     const dispatch = useDispatch();
     const menu = useSelector(state => state.menu.menu);
+    const cart = useSelector(state => state.cart);
 
     useEffect(() => {
         dispatch(getMenu);
@@ -28,7 +29,11 @@ function Home() {
                         </div>
                     </header>
                     <div className="menu">
+                        {console.log('cart.....', cart)}
+
                     <div className="cart-text" onClick={() => setIsVisible(true)}><b>View Cart</b></div>
+                    <div className="cart-counter" onClick={() => setIsVisible(true)}>{cart.cart.length}</div>
+
                     <div className="cart" onClick={() => setIsVisible(true)}>View Cart</div>
                     {/* For each category we have, render out a MenuCard */}
                     {menu !== undefined && menu.menu?.map((item) =>
