469972e6d967faef8fe8001bed6fc530fc639d20
Rihana Chang <martinson.r@gmail.com>
2022-06-16 13:11:05 -0700

    Begin setting up home page

---
+++ src/components/Home/index.js
@@ -0,0 +1,13 @@
+import { useEffect, useState } from "react";
+import { useSelector, useDispatch } from "react-redux";
+import "./home.css";
+
+function Home() {
+
+    return (
+        <></>
+    );
+
+}
+
+export default Home;
